afa88e7d35ba31588eb17dae7b0633baf18b653a
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2025-03-01 11:36:40 +0530

    first commit

---
+++ index.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int nums[]={3,5,1,9,3450,12,14,56,18,0};
+    int size=sizeof(nums)/sizeof(nums[0]);
+    int smallest=INT16_MAX;
+    int greatest=INT16_MIN;
+
+    for(int i=0;i<size;i++){
+        // if(nums[i]<smallest){
+        //     smallest=nums[i];
+        // }
+        smallest=min(nums[i],smallest);
+    }
+
+    for(int i=0;i<size;i++){
+        // if(nums[i]>greatest){
+        //     greatest=nums[i];
+        // }
+        greatest=max(nums[i],greatest);
+    }
+    cout<<"your smallest number is:"<<smallest<<endl;
+    cout<<"your greatest number is:"<<greatest;
+}
